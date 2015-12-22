677a400b5ea16c7877575e4060a9b25cf622688d
Jeong Kim <joshkim83@outlook.com>
2015-12-22 09:20:16 -0500

    updates session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,13 @@ class SessionsController < ApplicationController
         session[:server_id] = server.id
         redirect_to orders_path
       else
-        redirect_to log_in_path
+        redirect_to root_path
       end
     end
 
     def destroy
       session[:server_id] = nil
-      redirect_to log_in_path
+      redirect_to root_path
     end
 
 end
