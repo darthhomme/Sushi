e94b01236713b5741ed2e599d3410ca012309364
Jeong Kim <joshkim83@outlook.com>
2016-01-23 09:34:24 -0500

    added some views

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,18 @@
+class SessionController < ApplicationController
+  def new
+  end
+  def create
+    employee = Employee.find_by_username(params[:username])
+    if employee && employee.authenticate(params[:password])
+      session[:employee_id] = employee.id
+      redirect_to employees_path
+    else
+      redirect_to log_in_path
+    end
+  end
+
+  def destroy
+    session[:employee_id] = nil
+    redirect_to log_in_path
+  end
+end
