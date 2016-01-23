59846cdda6c895756302cce5d6dd87442237cae8
Jeong Kim <joshkim83@outlook.com>
2016-01-23 12:00:16 -0500

    adds styles and logic

--- app/assets/javascripts/parties.js
+++ app/assets/javascripts/parties.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+console.log("ready");
+$("#open-modal").on('click', function(){
+$('#modal').toggle();
+})
+$("#close-modal").on('click', function(){
+$('#modal').toggle();
+var $imgUrl = $('#img-url');
+$imgUrl.val('');
+})
+$("#submit-btn").on('click', function(){
+//grab value of input box
+var $imgUrl = $('#img-url');
+//creting image tag src = value of that input box
+var $imgTag = $('<img>').attr('src', $imgUrl.val());
+//clear value of input box
+$imgUrl.val('');
+//close our modal
+$('#modal').toggle();
+//image tag appends to the body
+$('body').append($imgTag);
+})
